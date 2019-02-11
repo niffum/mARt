77129b655ffb05e748dadfc84d5602c11848ea35
Viola <i_like_mini_muffins@web.de>
2019-02-11 18:23:22 +0100

    2D scene to view images in 2d with leap, scrolling per wheel, setting contrast and brightness

---
+++ mARt/Assets/Scripts/main2D/ScrollImages.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+using System.IO;
+using System;
+
+public class ScrollImages : MonoBehaviour {
+
+    [SerializeField]
+    private string folder;
+
+    private List<Texture2D> images = new List<Texture2D>();
+
+    private static readonly string[] ValidImageFileExtensions = { ".jpg", ".png" };
+
+
+    private int depth;
+
+    [SerializeField]
+    private TextMesh currentdepth;
+
+    [SerializeField]
+    private TextMesh scrollBytext;
+
+    private Material material;
+
+    private bool scrollForward = true;
+
+    int lastScrollBy;
+
+    [SerializeField]
+    float DragFactor = 10f;
+
+    [SerializeField]
+    float DragSpeed = 1.5f;
+
+    //[SerializeField]
+    //private ScrollBarManager scrollBar;
+
+    Vector3 lastPosition;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        AddImagesToList();
+
+        depth = 0;
+        material.SetTexture("_MainTex", images[0]);
+
+        //scrollBar.SetMaxDepth(images.Count);
+    }
+
+    public void Scroll(float zRotation)
+    {
+        int newDepth = (int)MapValue(0, 360, 0, images.Count, (int)zRotation);
+        //int newDepth =(int) (0.5 * zRotation + images.Count / 2);
+        Debug.Log("depth: " + newDepth);
+        if (depth < images.Count - 1 && newDepth > 0)
+        {
+            ChangeCanvasImage(newDepth);
+        }
+        /*
+        int scrollBy = (int)zRotation;
+
+        if (lastScrollBy != scrollBy && scrollBy != 0 && Math.Abs(scrollBy) >=3)
+        {
+            
+            if (scrollBy > 0)
+            {
+                if (depth < images.Count - 1)
+                {
+                    ChangeCanvasImage(depth + 1);
+                }
+            }
+            else
+            {
+                if (depth > 0)
+                {
+                    ChangeCanvasImage(depth - 1);
+                }
+            }
+            lastScrollBy = scrollBy;
+        }
+        */
+    }
+
+    public void ChangeCanvasImage(int newDepth)
+    {
+        depth = newDepth;
+        currentdepth.text = depth + "/" + images.Count;
+        material.SetTexture("_MainTex", images[depth]);
+        //scrollBar.SetCurrentDepth(newDepth);
+    }
+
+    private void AddImagesToList()
+    {
+        var imageNames = GetImagesInFolder("" + Application.streamingAssetsPath + folder);
+        Debug.LogWarning(Application.streamingAssetsPath + folder);
+        foreach (var imageFile in imageNames)
+        {
+            var tex = new Texture2D(2, 2);
+            bool loaded = tex.LoadImage(File.ReadAllBytes(imageFile));
+            images.Add(tex);
+        }
+    }
+    private static string[] GetImagesInFolder(string folder)
+    {
+        return Directory.GetFiles(folder)
+                        .Where(k => IsFileAnImage(k))
+                        .OrderBy(k => k.ToLower())
+                        .ToArray();
+    }
+
+    private static bool IsFileAnImage(string file)
+    {
+        var fileLower = file.ToLower();
+        return ValidImageFileExtensions.Any(k => fileLower.EndsWith(k));
+    }
+
+    public float MapValue(int a0, int a1, int b0, int b1, int a)
+    {
+        return b0 + (b1 - b0) * ((a - a0) / (a1 - a0));
+    }
+}
---
+++ mARt/Assets/Shaders/TextureBrightnessContrast.shader
@@ -0,0 +1,82 @@
+﻿Shader "Unlit/TextureBrightnessContrast"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Brightness("Brightness", Range(0,1)) = 0.5
+		_Contrast("Contrast", Range(0,1)) = 0.5
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+
+			float _Contrast, _Brightness;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+
+			inline float4 applyContrastAndBrightness(float4 startColor, float contrast, float brightness)
+			{
+				//https://forum.unity.com/threads/hue-saturation-brightness-contrast-shader.260649/
+
+				
+				brightness = brightness * 2 - 1;
+				contrast = contrast * 2;
+				
+				//float _Saturation = hsbc.a * 2;
+
+				float4 outputColor = startColor;
+				outputColor.rgb = (outputColor.rgb - 0.5f) * (contrast)+0.5f;
+				outputColor.rgb = outputColor.rgb + brightness;
+				/*
+				float3 intensity = dot(outputColor.rgb, float3(0.299, 0.587, 0.114));
+				outputColor.rgb = lerp(intensity, outputColor.rgb, _Saturation);
+				*/
+				return outputColor;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = applyContrastAndBrightness( tex2D(_MainTex, i.uv), _Contrast, _Brightness);
+				//fixed4 col = tex2D(_MainTex, i.uv);
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
