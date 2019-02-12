bfa90ca34ade951a02ec39062fffaa74af8ef830
Viola <i_like_mini_muffins@web.de>
2019-02-12 12:57:38 +0100

    Added scale and translate script to 2d scene, commit of icons in 2d scene

---
+++ mARt/Assets/Scripts/main2D/KeepOriginPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepOriginPosition : MonoBehaviour {
+
+    private Vector3 originPosition;
+	
+	void Start () {
+        originPosition = transform.position;
+	}
+	
+	public void ReturnToOriginPosition()
+    {
+        transform.position = originPosition;
+    }
+}
---
+++ mARt/Assets/Scripts/main2D/LeapPinchDrag.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+
+
+public class LeapPinchDrag : MonoBehaviour {
+
+    [SerializeField]
+    private PinchDetector _pinchDetectorA;
+    public PinchDetector PinchDetectorA
+    {
+        get
+        {
+            return _pinchDetectorA;
+        }
+        set
+        {
+            _pinchDetectorA = value;
+        }
+    }
+
+    [SerializeField]
+    private PinchDetector _pinchDetectorB;
+    public PinchDetector PinchDetectorB
+    {
+        get
+        {
+            return _pinchDetectorB;
+        }
+        set
+        {
+            _pinchDetectorB = value;
+        }
+    }
+
+  
+    private Transform _anchor;
+
+    [SerializeField]
+    private LeapPinchScale scaling;
+    
+
+    void Start()
+    {
+        //      if (_pinchDetectorA == null || _pinchDetectorB == null) {
+        //        Debug.LogWarning("Both Pinch Detectors of the LeapRTS component must be assigned. This component has been disabled.");
+        //        enabled = false;
+        //      }
+
+        GameObject pinchControl = new GameObject("RTS Anchor");
+        _anchor = pinchControl.transform;
+        _anchor.transform.parent = transform.parent;
+        transform.parent = _anchor;
+    }
+
+    void Update()
+    {
+        
+        bool didUpdate = false;
+        if (_pinchDetectorA != null)
+            didUpdate |= _pinchDetectorA.DidChangeFromLastFrame;
+        if (_pinchDetectorB != null)
+            didUpdate |= _pinchDetectorB.DidChangeFromLastFrame;
+
+        if (didUpdate)
+        {
+            transform.SetParent(null, true);
+        }
+
+        if (_pinchDetectorA != null && _pinchDetectorA.IsPinching)
+        {
+            transformSingleAnchor(_pinchDetectorA);
+        }
+        else if (_pinchDetectorB != null && _pinchDetectorB.IsPinching)
+        {
+            transformSingleAnchor(_pinchDetectorB);
+        }
+
+        if (didUpdate)
+        {
+            transform.SetParent(_anchor, true);
+        }
+    }
+
+ 
+    private void transformSingleAnchor(PinchDetector singlePinch)
+    {
+        if (!scaling.scalingInProcess)
+        {
+            _anchor.position = singlePinch.Position;
+            
+        }
+    }
+}
