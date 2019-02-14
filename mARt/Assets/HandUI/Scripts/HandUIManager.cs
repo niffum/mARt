6e781c86a1c6c31acb0dd67610271982f882f62e
Viola <i_like_mini_muffins@web.de>
2019-02-15 00:16:58 +0100

    Added Hand UI menu with data list to 2D scene

---
+++ mARt/Assets/HandUI/Scripts/HandUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Animation;
+
+public class HandUIManager : MonoBehaviour {
+
+    [SerializeField]
+    private TransformTweenBehaviour dataListTween;
+
+    private bool dataListIsVisible = false;
+
+    private void Start()
+    {
+        dataListTween.PlayBackward();
+        dataListIsVisible = false;
+    }
+
+    public void ToggleDataList()
+    {
+        if(!dataListIsVisible)
+        {
+            dataListTween.PlayForward();
+            dataListIsVisible = true;
+        }
+        else
+        {
+            dataListTween.PlayBackward();
+            dataListIsVisible = false;
+        }
+    }
+}
