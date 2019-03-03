82bc6dc0a66590be8d427ad0406e2c0b024c4024
Viola <i_like_mini_muffins@web.de>
2019-03-03 22:29:28 +0100

    Removed unnecessary wheel rotation code

--- mARt/Assets/2DUI/Scripts/RotateDiscInteraction.cs
+++ mARt/Assets/2DUI/Scripts/RotateDiscInteraction.cs
@@ -14,15 +14,8 @@ public class RotateDiscInteraction : MonoBehaviour
 
     private Material _material;
 
-    public Color defaultColor = Color.Lerp(Color.black, Color.white, 0.1F);
-    public Color primaryHoverColor = Color.Lerp(Color.black, Color.white, 0.8F);
-    public Color hoverColor = Color.Lerp(Color.black, Color.white, 0.7F);
-
     public Color pressedColor = Color.white;
     
-    [SerializeField]
-    private TextMesh text;
-
     Vector3 handPos = Vector3.zero;
 
     [SerializeField]
@@ -36,16 +29,11 @@ public class RotateDiscInteraction : MonoBehaviour
     bool firstTouch = false;
 
     private InteractionController currentController;
-
-    [HideInInspector]
-    public ManipulateImages scroll;
-
+    
     private float rotationCounter;
 
     public Action<float> OnScroll;
-
-    private Collider wheelCollider;
-
+    
     void Start()
     {
         _intObj = GetComponent<InteractionBehaviour>();
@@ -63,51 +51,9 @@ public class RotateDiscInteraction : MonoBehaviour
         _intObj.OnContactEnd += EndTouch;
         _intObj.OnContactBegin += StartToch;
 
-        wheelCollider = GetComponent<Collider>();
     }
 
-    void Update()
-    {
-        if (_material != null)
-        {
-
-            // The target color for the Interaction object will be determined by various simple state checks.
-            Color targetColor = defaultColor;
-
-            // "Primary hover" is a special kind of hover state that an InteractionBehaviour can
-            // only have if an InteractionHand's thumb, index, or middle finger is closer to it
-            // than any other interaction object.
-            if (_intObj.isPrimaryHovered)
-            {
-                //targetColor = primaryHoverColor;
-            }
-            else
-            {
-                // Of course, any number of objects can be hovered by any number of InteractionHands.
-                // InteractionBehaviour provides an API for accessing various interaction-related
-                // state information such as the closest hand that is hovering nearby, if the object
-                // is hovered at all.
-                if (_intObj.isHovered )
-                {
-                    float glow = _intObj.closestHoveringControllerDistance.Map(0F, 0.2F, 1F, 0.0F);
-                    targetColor = Color.Lerp(defaultColor, hoverColor, glow);
-                }
-            }
-
-            // We can also check the depressed-or-not-depressed state of InteractionButton objects
-            // and assign them a unique color in that case.
-            if (_intObj is InteractionButton && (_intObj as InteractionButton).isPressed)
-            {
-                //targetColor = pressedColor;
-                //OnRotation();
-            }
-            
-            // Lerp actual material color to the target color.
-            _material.color = Color.Lerp(_material.color, targetColor, 30F * Time.deltaTime);
-        }
-
-        
-    }
+    //_material.color = Color.Lerp(_material.color, targetColor, 30F * Time.deltaTime);
 
     private void OnTriggerStay(Collider other)
     {
@@ -169,15 +115,13 @@ public class RotateDiscInteraction : MonoBehaviour
             // Lerp?
             cylinder.transform.RotateAroundLocal(Vector3.forward, rotationAmount * SPEED_DAMPER);
 
-            rotationCounter += rotationAmount * SPEED_DAMPER;
+            //rotationCounter += rotationAmount * SPEED_DAMPER;
 
-            //scroll.Scroll(rotationAmount);
               
             if(OnScroll != null)
             {
                 OnScroll(rotationAmount);
             }
-            //text.text = "" + rotationAmount;
         }
 
     }
